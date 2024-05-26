a38ddefce01530e12c4b8ba2361b5b3a89558ed6
esraa-mohamad <78769599+esraa-mohamad@users.noreply.github.com>
2024-05-27 00:24:49 +0300

    using cached network image

--- lib/Features/home/presentation/views/widgets/custom_book_item_image.dart
+++ lib/Features/home/presentation/views/widgets/custom_book_item_image.dart
@@ -1,23 +1,30 @@
+import 'package:bookly/Core/widgets/custom_loading_indicator.dart';
 import 'package:bookly/constants.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 class CustomBookItemImage extends StatelessWidget {
   const CustomBookItemImage({super.key, required this.imageUrl});
 
-  final String imageUrl ;
+  final String imageUrl;
 
   @override
   Widget build(BuildContext context) {
-    return AspectRatio(
-      aspectRatio: 2.6/4,
-      child: Container(
-        decoration:  BoxDecoration(
-          color: Colors.red,
-          borderRadius: BorderRadius.circular(ConstantsSize.s18),
-          image:  DecorationImage(
-            fit: BoxFit.fill,
-            image: NetworkImage(imageUrl),
-          )
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(ConstantsSize.s16),
+      child: AspectRatio(
+        aspectRatio: 2.6 / 4,
+        child: CachedNetworkImage(
+          fit: BoxFit.fill,
+          imageUrl: imageUrl,
+          errorWidget: (context, url, error) => const Icon(
+            Icons.error,
+            size: ConstantsSize.s30,
+            color: Colors.red,
+          ),
+          placeholder: (context, url) => const Center(
+            child: CustomLoadingIndicator(),
+          ),
         ),
       ),
     );
